6b53f1640c6158896b3479520cedc15cef4fda65
Kincses Dániel <danikincs@gmail.com>
2026-06-06 11:15:53 +0200

    Add Tanstack Query provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import { createRouter, RouterProvider } from "@tanstack/react-router"
 import { routeTree } from "./routeTree.gen.ts"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 const router = createRouter({ routeTree })
+const queryClient = new QueryClient()
 
 declare module "@tanstack/react-router" {
     interface Register {
@@ -14,6 +16,8 @@ declare module "@tanstack/react-router" {
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
     </StrictMode>,
 )
