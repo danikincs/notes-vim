c6e77194d0c9b10398ab3c7ca0e46436d4e04cad
Kincses Dániel <danikincs@gmail.com>
2026-06-06 11:23:57 +0200

    Use TanStack Query for notes

--- src/routes/notes.tsx
+++ src/routes/notes.tsx
@@ -1,9 +1,30 @@
+import { useQuery } from "@tanstack/react-query"
 import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/notes")({
     component: NotesPage,
 })
 
+const mockNotes = [
+    "Learn tanstack query",
+    "Add tankstack query",
+    "Build notes app",
+]
+
 function NotesPage() {
-    return <h1>Notes</h1>
+    const notesQuery = useQuery({
+        queryKey: ["notes"],
+        queryFn: async () => mockNotes,
+    })
+
+    return (
+        <main>
+            <h1>Notes</h1>
+            <ul>
+                {notesQuery.data?.map((note) => (
+                    <li key={note}>{note}</li>
+                ))}
+            </ul>
+        </main>
+    )
 }
