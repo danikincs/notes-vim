42f83177243204909242fef4c3584c6ddf912957
Kincses Dániel <danikincs@gmail.com>
2026-06-06 11:40:54 +0200

    Add register form

--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -1,3 +1,4 @@
+import { useForm } from "@tanstack/react-form"
 import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/register")({
@@ -5,5 +6,69 @@ export const Route = createFileRoute("/register")({
 })
 
 function RegisterPage() {
-    return <h1>Register</h1>
+    const form = useForm({
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+        },
+        onSubmit: ({ value }) => {
+            console.log(value)
+        },
+    })
+
+    return (
+        <main>
+            <h1>Register</h1>
+
+            <form
+                onSubmit={(event) => {
+                    event.preventDefault()
+                    form.handleSubmit()
+                }}
+            >
+                <form.Field name="name">
+                    {(field) => (
+                        <label>
+                            name
+                            <input
+                                value={field.state.value}
+                                onChange={(event) =>
+                                    field.handleChange(event.target.value)
+                                }
+                            />
+                        </label>
+                    )}
+                </form.Field>
+                <form.Field name="email">
+                    {(field) => (
+                        <label>
+                            Email
+                            <input
+                                value={field.state.value}
+                                onChange={(event) =>
+                                    field.handleChange(event.target.value)
+                                }
+                            />
+                        </label>
+                    )}
+                </form.Field>
+                <form.Field name="password">
+                    {(field) => (
+                        <label>
+                            Password
+                            <input
+                                type="password"
+                                value={field.state.value}
+                                onChange={(event) =>
+                                    field.handleChange(event.target.value)
+                                }
+                            />
+                        </label>
+                    )}
+                </form.Field>
+                <button type="submit">Register</button>
+            </form>
+        </main>
+    )
 }
