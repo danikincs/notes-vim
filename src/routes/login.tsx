e2991a5943c4a14038bd6bc883a9b49c26aefe1b
Kincses Dániel <danikincs@gmail.com>
2026-06-06 11:36:04 +0200

    Add login form

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,4 @@
+import { useForm } from "@tanstack/react-form"
 import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/login")({
@@ -5,5 +6,53 @@ export const Route = createFileRoute("/login")({
 })
 
 function LoginPage() {
-    return <h1>Login</h1>
+    const form = useForm({
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+        onSubmit: ({ value }) => {
+            console.log(value)
+        },
+    })
+    return (
+        <main>
+            <h1>Login</h1>
+            <form
+                onSubmit={(event) => {
+                    event.preventDefault()
+                    form.handleSubmit()
+                }}
+            >
+                <form.Field name="email">
+                    {(field) => (
+                        <label>
+                            Email
+                            <input
+                                value={field.state.value}
+                                onChange={(event) =>
+                                    field.handleChange(event.target.value)
+                                }
+                            />
+                        </label>
+                    )}
+                </form.Field>
+                <form.Field name="password">
+                    {(field) => (
+                        <label>
+                            Password
+                            <input
+                                type="password"
+                                value={field.state.value}
+                                onChange={(event) =>
+                                    field.handleChange(event.target.value)
+                                }
+                            />
+                        </label>
+                    )}
+                </form.Field>
+                <button type="submit">Login</button>
+            </form>
+        </main>
+    )
 }
